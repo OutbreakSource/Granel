d44a81a8fc0792201b45e7a6f1359acf6d0d8126
daniel <daniel_ends@yahoo.com>
2025-01-18 13:47:58 -0700

    -created component/screen, optimizing right now to replace or remove -Custom screen to look at plans for selection created TODO: add functionality to select the plan and make it just your one main plan on main page

--- src/components/MainPlanSelection.js
+++ src/components/MainPlanSelection.js
@@ -1,41 +1,32 @@
-const MainPlanSelection = ({navigation, id, theme, currentDay, equipment}) => {
-    let routines = [["Rest",
-        "Chest/Lats/Lower Back/Middle Back/Shoulders/Triceps/Biceps/Forearms",
-        "Chest/Lats/Lower Back/Middle Back/Shoulders/Triceps/Biceps/Forearms/Calves/Glutes/Hamstrings/Quadriceps/Abdominals",
-        "Calves/Glutes/Hamstrings/Quadriceps/Abdominals",
-        "Chest/Lats/Lower Back/Middle Back/Shoulders/Triceps/Biceps/Forearms/Calves/Glutes/Hamstrings/Quadriceps/Abdominals",
-        "Chest/Lats/Lower Back/Middle Back/Shoulders/Triceps/Biceps/Forearms/Calves/Glutes/Hamstrings/Quadriceps/Abdominals",
-        "Your Choice"
-    ],["Rest",
-        "Quadriceps/Glutes/Abdominals",
-        "Lats/Chest",
-        "Calves/Glutes/Hamstrings/Quadriceps/Core",
-        "Lower back/Middle back/Triceps",
-        "Shoulders",
-        "Chest/Biceps/Abdominals"
-    ],["Calves/Glutes/Hamstrings/Quadriceps",
-        "Chest",
-        "Biceps",
-        "Legs",
-        "Triceps",
-        "Shoulders",
-        "Lower back/Middle back"
-    ],["Rest",
-        "Chest",
-        "Biceps/Triceps",
-        "Legs",
-        "Lower back/Middle back",
-        "Shoulders",
-        "Rest"
-    ],["Rest",
-        "Lower back/Middle back/Triceps",
-        "Chest/Biceps/Abdominals",
-        "Calves/Glutes/Hamstrings/Quadriceps/Shoulder",
-        "Lower back/Middle back/Lats/Triceps",
-        "Shoulders/Chest/Biceps",
-        "Rest"
-    ]]
+import {View} from "react-native";
 
+const MainPlanSelection = ({navigation, setSelectedItems, selectedItems}) => {
+
+    const onSelectedItemsChange = (newSelectedItems) => {
+        setSelectedItems(newSelectedItems);
+    };
+
+    const createViews = () => {
+        return routines.map((group, index) => (
+            <View style={styles.viewBlock} key={index}>
+                <View>
+                    <Text style={styles.textStyle}>{group}</Text>
+                </View>
+                <View style={styles.imageContainer}>
+                    <Image
+                        key={index}
+                        source={{ uri: `${imageUrls[index]}&timestamp=${Date.now()}` }}
+                        style={{ width: '100%', height: '100%', borderRadius: 25, resizeMode: "contain"}}
+                    />
+                </View>
+            </View>
+
+        ));
+    };
+
+    return (
+        <View>{createViews()}</View>
+    )
 
 
 }
--- src/screens/SelectionScreen.js
+++ src/screens/SelectionScreen.js
@@ -1,8 +1,127 @@
+import {ScrollView, View, Text, Image, StyleSheet, Dimensions} from "react-native";
+import React from "react";
+
 const SelectionScreen = ({navigation}) => {
+    let routines = [["Rest",
+        "Chest/Lats/Lower Back/Middle Back/Shoulders/Triceps/Biceps/Forearms",
+        "Chest/Lats/Lower Back/Middle Back/Shoulders/Triceps/Biceps/Forearms/Calves/Glutes/Hamstrings/Quadriceps/Abdominals",
+        "Calves/Glutes/Hamstrings/Quadriceps/Abdominals",
+        "Chest/Lats/Lower Back/Middle Back/Shoulders/Triceps/Biceps/Forearms/Calves/Glutes/Hamstrings/Quadriceps/Abdominals",
+        "Chest/Lats/Lower Back/Middle Back/Shoulders/Triceps/Biceps/Forearms/Calves/Glutes/Hamstrings/Quadriceps/Abdominals",
+        "Your Choice"
+    ],["Rest",
+        "Quadriceps/Glutes/Abdominals",
+        "Lats/Chest",
+        "Calves/Glutes/Hamstrings/Quadriceps/Core",
+        "Lower back/Middle back/Triceps",
+        "Shoulders",
+        "Chest/Biceps/Abdominals"
+    ],["Calves/Glutes/Hamstrings/Quadriceps",
+        "Chest",
+        "Biceps",
+        "Legs",
+        "Triceps",
+        "Shoulders",
+        "Lower back/Middle back"
+    ],["Rest",
+        "Chest",
+        "Biceps/Triceps",
+        "Legs",
+        "Lower back/Middle back",
+        "Shoulders",
+        "Rest"
+    ],["Rest",
+        "Lower back/Middle back/Triceps",
+        "Chest/Biceps/Abdominals",
+        "Calves/Glutes/Hamstrings/Quadriceps/Shoulder",
+        "Lower back/Middle back/Lats/Triceps",
+        "Shoulders/Chest/Biceps",
+        "Rest"
+    ]]
 
+    function getDay(dayCurr) {
+        if (dayCurr === 0){
+            return "Sunday"
+        }
+        else if (dayCurr === 1){
+            return "Monday"
+        }
+        else if (dayCurr === 2){
+            return "Tuesday"
+        }
+        else if (dayCurr === 3){
+            return "Wednesday"
+        }
+        else if (dayCurr === 4){
+            return "Thursday"
+        }
+        else if (dayCurr === 5){
+            return "Friday"
+        }
+        else if (dayCurr === 6){
+            return "Saturday"
+        }
+    }
 
+    const createViews = () => {
+        return routines.map((routine, routineIndex) => (
+            <View style={styles.viewBlock} key={routineIndex}>
+                <View>
+                    {
+                        routine.map((plan, planIndex) => (
+                            <View>
+                                <Text>
+                                    {getDay(planIndex)}.
+                                </Text>
+                                <Text style={styles.textStyle} key={planIndex}>
+                                    {plan}
+                                </Text>
+                            </View>
+                        ))
+                    }
+                </View>
+            </View>
+
+        ));
+    };
+
+    return (
+        <ScrollView horizontal
+                    pagingEnabled>
+            {createViews()}
+        </ScrollView>
+    )
 
 }
+
+const { width, height } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontFamily: 'Verdana',
+        fontSize: 20,
+        textDecorationStyle: "double",
+        alignSelf: "center",
+        textTransform: "uppercase",
+        lineHeight: 30
+    },
+    viewBlock: {
+        backgroundColor: '#4fbded',
+        borderRadius: 40,
+        justifyContent: 'space-between',
+        width: width,
+        height: height * 0.80,
+        paddingTop: height * 0.05,
+
+    },
+
+    slider: {
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
+
 
 
 
